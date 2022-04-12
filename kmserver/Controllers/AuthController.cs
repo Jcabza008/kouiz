44fe7537a5adfeceaab49c4a5ac8e87575e4625e
Julio J. Cabrera <54811155+jjcabrera-terainsights@users.noreply.github.com>
2022-04-11 20:48:56 -0400

    wip

---
+++ kmserver/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace kmserver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> _userManager)
+        {
+            userManager = _userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ kmserver/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kmserver.Models
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+
+    }
+}
