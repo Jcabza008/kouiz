ad0e5ef4c50560c760fbdd50d0c5a28d9855363a
Julio J. Cabrera <jcabza008@gmail.com>
2022-04-19 10:54:31 -0400

    created workflows for testing and version management

---
+++ scripts/ci/setup-env-ubuntu.sh
@@ -0,0 +1,43 @@
+
+#!/usr/bin/env bash
+
+#set -x # echo on
+SUDO=sudo
+if [ "$GITHUB_ACTOR" == "nektos/act" ]; then
+    echo "Running in 'act' container, not using sudo"
+    SUDO=""
+
+    # workaround for act nodeos-based container
+    # see https://github.com/nektos/act/issues/251
+    if [ ! -f "/etc/lsb-release" ]; then
+        echo "Manually creating lsb-release file"
+        echo "DISTRIB_RELEASE=18.04" > /etc/lsb-release
+    fi
+fi
+
+if [ -n "$GITHUB_EVENT_PATH" ]; then
+    echo "Printing action event JSON file:"
+    cat $GITHUB_EVENT_PATH;
+    echo ""
+else
+    echo "GITHUB_EVENT_PATH is not set!";
+fi
+
+echo "Dumping environment:"
+printenv
+
+echo "Updating apt sources:"
+$SUDO apt-get update
+
+echo "Installing packages:"
+$SUDO apt-get install -y    \
+    gcc                 \
+    libssl-dev          \
+    pkg-config          \
+    libcryptsetup-dev   \
+    tree                \
+    libc6               \
+    git
+
+echo "Dumping working tree structure:"
+tree -Dphug
